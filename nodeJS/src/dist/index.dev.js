aa812a5e1826f0cada57b8d256894abb8de282c1
Adrián Serrano <adrianssc123@gmail.com>
2020-04-08 20:00:44 +0200

    update

--- nodeJS/src/dist/index.dev.js
+++ nodeJS/src/dist/index.dev.js
@@ -11,8 +11,18 @@ app.set("env", "development");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.get("/", function (req, res) {
-  res.render('home');
+  res.render('home', {
+    title: "Curso de nodeJS",
+    message: "mi segundo renderizado de plantillas, esta vez von variables"
+  });
 });
+app.get("/article", function (req, res) {
+  res.render('article', {
+    title: "Articulo del curso de nodeJS",
+    message: "Esta es la página del articulo del curso de nodeJS"
+  });
+});
+app.use('/static', express["static"](path.join(__dirname, "public")));
 app.listen(port, function () {
   console.log("Server listen in port ".concat(port));
 });
